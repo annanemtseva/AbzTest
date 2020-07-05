4d9a0e53008bbf7e3945eaa5e23d6265c5b6dc42
annanemtseva <nemtseva.anna@gmail.com>
2020-07-05 17:45:12 +0300

    add butstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
